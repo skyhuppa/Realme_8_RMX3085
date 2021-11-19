e4a580e005244cd965800261349cf97f31e39eab
Adasi_Kaakyire <64362452+skyhuppa@users.noreply.github.com>
2021-11-19 23:29:41 +0000

    Update omni_RMX3085.mk

--- omni_RMX3085.mk
+++ omni_RMX3085.mk
@@ -6,9 +6,9 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/pb/config/common.mk)
@@ -16,10 +16,10 @@ $(call inherit-product, vendor/pb/config/common.mk)
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := RMX3191
-PRODUCT_NAME := omni_RMX3191
-PRODUCT_BRAND := Realme
-PRODUCT_MODEL := C25
+PRODUCT_DEVICE := RMX3085
+PRODUCT_NAME := omni_RMX3085
+PRODUCT_BRAND := RMX3085
+PRODUCT_MODEL := Realme 8
 PRODUCT_MANUFACTURER := Realme
 
 # Dynamic
