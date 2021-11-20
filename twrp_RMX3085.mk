5ecb54028faf4fcd375e67a97e1377c2194bb0fd
Adasi_Kaakyire <64362452+skyhuppa@users.noreply.github.com>
2021-11-20 03:47:56 -1200

    Update twrp_RMX3085.mk

--- twrp_RMX3085.mk
+++ twrp_RMX3085.mk
@@ -13,8 +13,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Inherit some common Omni stuff.
  $(call inherit-product, vendor/twrp/config/common.mk)
 
-# PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root) \
-#	 $(LOCAL_PATH)/prebuilt/dtb:dtb.img
+  PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root) \
+	 $(LOCAL_PATH)/prebuilt/dtb:dtb.img
  
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := RMX3085
