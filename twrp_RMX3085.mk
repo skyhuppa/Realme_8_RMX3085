82f2e05a2524dd84a6658319f3e21a16906458df
Adasi_Kaakyire <64362452+skyhuppa@users.noreply.github.com>
2021-11-21 05:51:06 -1200

    Update twrp_RMX3085.mk

--- twrp_RMX3085.mk
+++ twrp_RMX3085.mk
@@ -31,6 +31,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
+    
+PRODUCT_SHIPPING_API_LEVEL := 30    
 
 # HACK: Set vendor patch level
 PRODUCT_PROPERTY_OVERRIDES += \
