1058a50b57490f2ff6fc1cd5d5d7c6745e8ee160
Adasi_Kaakyire <64362452+skyhuppa@users.noreply.github.com>
2021-11-21 01:22:22 -1200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -141,7 +141,7 @@ BOARD_HAS_NO_SELECT_BUTTON := true
      ashmemd_aidl_interface-cpp \
      libashmemd_client
 
-#TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+  TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
    $(TARGET_OUT_SHARED_LIBRARIES)/ashmemd_aidl_interface-cpp.so \
