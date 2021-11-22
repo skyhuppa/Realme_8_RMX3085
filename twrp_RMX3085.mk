e87e319cf3de1c6a48fad4e609e83123bf65cc98
Adasi_Kaakyire <64362452+skyhuppa@users.noreply.github.com>
2021-11-22 04:52:28 -1200

    Update twrp_RMX3085.mk

--- twrp_RMX3085.mk
+++ twrp_RMX3085.mk
@@ -6,19 +6,19 @@
 #
 
 # Inherit from those products. Most specific first.
-# $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-# $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit some common Omni stuff.
- $(call inherit-product, vendor/twrp/config/common.mk)
+ $(call inherit-product, vendor/omni/config/common.mk)
 
   PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root) \
  	 $(LOCAL_PATH)/prebuilt/dtb:dtb.img
  
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := RMX3085
-PRODUCT_NAME := twrp_RMX3085
+PRODUCT_NAME := omni_RMX3085
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX3085
 PRODUCT_MANUFACTURER := realme
