4e1731639575c5a2d0b1069095c26dcd2d2b0030
Adasi_Kaakyire <64362452+skyhuppa@users.noreply.github.com>
2021-12-05 13:55:58 +0000

    Update twrp_RMX3085.mk

--- twrp_RMX3085.mk
+++ twrp_RMX3085.mk
@@ -5,6 +5,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Release name
+PRODUCT_RELEASE_NAME := RMX3085
+DEVICE_PATH := device/realme/RMX3085
+
 # Inherit from those products. Most specific first.
 # $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
