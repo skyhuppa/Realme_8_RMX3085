f5478dbbac5621eabb1143c8e06bed24fccdedec
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2022-09-28 04:11:11 -0700

    Update twrp_RMX3085.mk

--- twrp_RMX3085.mk
+++ twrp_RMX3085.mk
@@ -13,10 +13,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 # Inherit some common Omni stuff.
  $(call inherit-product, vendor/twrp/config/common.mk)
 
-#  PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root) \
-# 	 $(LOCAL_PATH)/prebuilt/dtb:dtb.img
+  PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root) \
+ 	 $(LOCAL_PATH)/prebuilt/dtb:dtb.img
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.ko,$(DEVICE_PATH)/prebuilt,$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1)
+# PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.ko,$(DEVICE_PATH)/prebuilt,$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1)
  
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := RMX3085
