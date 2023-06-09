d73ace09fe4ab524f36d824a4625abed8bf7b7fe
Adasi Kaakyire <skyhuppa@hotmail.co.uk>
2023-06-09 09:04:34 -0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,10 +8,11 @@
 DEVICE_PATH := device/realme/RMX3085
 
 # For building with minimal manifest
- BUILD_BROKEN_DUP_RULES := true
-
-# Disable prebuilt ELF check by setting 
+# build: Disable prebuilt ELF check by setting
+ALLOW_MISSING_DEPENDENCIES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 BUILD_BROKEN_PREBUILT_ELF_FILES := true
+BUILD_BROKEN_DUP_RULES := true
 
 # Architecture
 TARGET_ARCH := arm64
