a2f2c78c7074834b32e2dd92a81da8ad5ee12ee1
Adasi Kaakyire <skyhuppa@hotmail.co.uk>
2023-06-25 07:53:45 -0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,63 +18,104 @@
 # 	Please maintain this if you use this script or any part of it
 #
 
-FOX_MANIFEST_ROOT=$(gettop)
-if [ -f $FOX_MANIFEST_ROOT/bootable/recovery/orangefox_defaults.go -a -f $FOX_MANIFEST_ROOT/bootable/recovery/orangefox.mk ]; then
-	#export PLATFORM_VERSION="11.0"
-	if [ -z "$TW_DEFAULT_LANGUAGE" ]; then
-		unset TW_DEFAULT_LANGUAGE
-		export TW_DEFAULT_LANGUAGE="en"
+FDEVICE="RMX3085"
+#set -o xtrace
 
+fox_get_target_device() {
+	local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+	if [ -n "$chkdev" ]; then
+		FOX_BUILD_DEVICE="$FDEVICE"
+	else
+		chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+		[ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
 	fi
-	export OF_KEEP_FORCED_ENCRYPTION=1
-	export OF_PATCH_AVB20=1
-	export OF_SCREEN_H=2400
-	export OF_STATUS_H=100
-	export OF_STATUS_INDENT_LEFT=48
-	export OF_STATUS_INDENT_RIGHT=48
-  	export OF_HIDE_NOTCH=1
-	export OF_CLOCK_POS=1
-	export OF_USE_MAGISKBOOT=1
-	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-        export OF_USE_TWRP_SAR_DETECT=1	
-	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-	#export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI=1; # if you disable this, then enable the next line
-	export OF_NO_MIUI_PATCH_WARNING=1
-	export OF_USE_GREEN_LED=0
+}
 
-	# use magisk 21.4 for the magisk addon
-	#export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-21.4.zip
+if [ -z "$1" ] && [ -z "$FOX_BUILD_DEVICE" ]; then
+	fox_get_target_device
+fi
 
-	export FOX_USE_BASH_SHELL=1
-	export FOX_ASH_IS_BASH=1
-	export FOX_USE_NANO_EDITOR=1
-	export FOX_USE_TAR_BINARY=1
-	export FOX_USE_ZIP_BINARY=1
-	export FOX_USE_SED_BINARY=1
-	export FOX_USE_XZ_UTILS=1
-	export FOX_REPLACE_BUSYBOX_PS=1
-	export OF_USE_NEW_MAGISKBOOT=1
-	#export FOX_BUGGED_AOSP_ARB_WORKAROUND="1510672800"; # Tue Nov 14 15:20:00 GMT 2017
+# Dirty Fix: Only declare orangefox vars when needed
+if [ -f "$(gettop)/bootable/recovery/orangefox.cpp" ]; then
+	echo -e "\x1b[96m[INFO]: Setting up OrangeFox build vars for rosemary...\x1b[m"
+	if [ "$1" = "$FDEVICE" ] || [  "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+		# Version / Maintainer infos
+		export OF_MAINTAINER="Skyhuppa"
+		export FOX_VERSION=R11.1_1
+		export FOX_BUILD_TYPE="Beta"
 
-	# OTA for custom ROMs
-	export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-	export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+		# Device info
+		export OF_AB_DEVICE=1
+		export OF_VIRTUAL_AB_DEVICE=1
+	#	export TARGET_DEVICE_ALT="secret, maltose"
+		
+		# OTA / DM-Verity / Encryption
+		export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+		export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+		
+		export OF_DONT_PATCH_ON_FRESH_INSTALLATION=1
+		export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+		export OF_KEEP_DM_VERITY_FORCED_ENCRYPTION=1
+		export OF_SKIP_FBE_DECRYPTION_SDKVERSION=34 # Don't try to decrypt A14(?)
+		export OF_SKIP_DECRYPTED_ADOPTED_STORAGE=1
 
-	# -- add settings for R11 --
-        export OF_MAINTAINER=Skyhuppa
-        export FOX_BUILD_TYPE="Stable"
-        export FOX_VARIANT=Realme 8
-	export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
-	export OF_QUICK_BACKUP_LIST="/boot;/data;"
-	# -- end R11 settings --
+		# Display / Leds
+		export OF_SCREEN_H="2400"
+		export OF_STATUS_H="100"
+		export OF_STATUS_INDENT_LEFT=48
+		export OF_STATUS_INDENT_RIGHT=48
+		export OF_HIDE_NOTCH=1
+		export OF_CLOCK_POS=1 # left and right clock positions available
+		export OF_USE_GREEN_LED=0
+		export OF_FL_PATH1="/tmp/flashlight" # See /init.recovery.mt6785.rc for more information
 
-	# Let's see which are our build vars
-    if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-        export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-        export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-        export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-        export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+		# Other OrangeFox configs
+		export OF_ENABLE_LPTOOLS=1
+		export OF_ALLOW_DISABLE_NAVBAR=0
+        export OF_QUICK_BACKUP_LIST="/boot;/data;"
+		export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800" # Tue Jan 1 2019 00:00:00 GMT
+		export FOX_DELETE_AROMAFM=1
+		export FOX_USE_SPECIFIC_MAGISK_ZIP="$(gettop)/device/redmi/rosemary/Magisk/Magisk.zip"
+
+        export BUNDLED_MAGISK_VER="25.2"
+        export BUNDLED_MAGISK_SUM="0bdc32918b6ea502dca769b1c7089200da51ea1def170824c2812925b426d509" # Sha256 sum of the prebuilt magisk
+
+            if [ -f "${FOX_USE_SPECIFIC_MAGISK_ZIP}" -a "$(sha256sum "${FOX_USE_SPECIFIC_MAGISK_ZIP}" 2>/dev/null | awk '{print $1}')" != "${BUNDLED_MAGISK_SUM}" ]
+            then
+                echo -e "\e[96m[INFO]: Removing invalid magisk zip\e[m"
+                rm -v "${FOX_USE_SPECIFIC_MAGISK_ZIP}"
+            fi
+
+        if [[ ! -f "${FOX_USE_SPECIFIC_MAGISK_ZIP}" ]]
+        then
+            # Download prebuilt magisk for OrangeFox builds
+            echo -e "\e[96m[INFO]: Downloading Magisk v${BUNDLED_MAGISK_VER}\e[m"
+            
+            if [[ "$(command -v "curl")" ]]
+            then
+                if [[ ! -d "$(dirname "${FOX_USE_SPECIFIC_MAGISK_ZIP}")" ]]
+                then
+                    mkdir -p "$(dirname "${FOX_USE_SPECIFIC_MAGISK_ZIP}")"
+                fi
+
+                # Download magisk and verify it
+                curl -L --progress-bar "https://github.com/topjohnwu/Magisk/releases/download/v${BUNDLED_MAGISK_VER}/Magisk-v${BUNDLED_MAGISK_VER}.apk" -o "${FOX_USE_SPECIFIC_MAGISK_ZIP}"
+                DOWNLOADED_SUM="$(sha256sum "${FOX_USE_SPECIFIC_MAGISK_ZIP}" | awk '{print $1}')"
+                
+                if [[ "${DOWNLOADED_SUM}" != "${BUNDLED_MAGISK_SUM}" ]]
+                then
+                    echo -e "\e[91m[ERROR]: Donwloaded Magisk ZIP seems *corrupted*, removing it to protect user's safety\e[m"
+                    rm "${FOX_USE_SPECIFIC_MAGISK_ZIP}"
+                    unset "FOX_USE_SPECIFIC_MAGISK_ZIP"
+                else
+                    echo -e "\e[96m[INFO]: Downloaded Magisk v${BUNDLED_MAGISK_VER}\e[m"
+                fi
+            else
+                # Curl is supposed to be installed according to "Establishing a build environnement" section in AOSP docs
+                # If it isn't, warn the builder about it and fallback to default Magisk ZIP
+                echo -e "\e[91m[ERROR]: Curl not found!\e[m"
+                unset "FOX_USE_SPECIFIC_MAGISK_ZIP"
+            fi
+        fi
     fi
 fi
