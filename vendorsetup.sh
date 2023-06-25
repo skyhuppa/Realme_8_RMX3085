4af50988a3ebfb09018787ba924281f37ae9afd7
Adasi Kaakyire <skyhuppa@hotmail.co.uk>
2023-06-25 15:08:16 -0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,16 +17,16 @@
 #
 # 	Please maintain this if you use this script or any part of it
 #
-FDEVICE="RMX3085"
+DEVICE="RMX3085"
 #set -o xtrace
 
 fox_get_target_device() {
-local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $DEVICE)
    if [ -n "$chkdev" ]; then 
-      FOX_BUILD_DEVICE="$FDEVICE"
+      FOX_BUILD_DEVICE="$DEVICE"
    else
-      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
-      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+      chkdev=$(set | grep BASH_ARGV | grep -w $DEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$DEVICE"
    fi
 }
 
@@ -34,7 +34,7 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
    fox_get_target_device
 fi
 
-if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+if [ "$1" = "$DEVICE" -o "$FOX_BUILD_DEVICE" = "$DEVICE" ]; then
    	export TW_DEFAULT_LANGUAGE="en"
 	export LC_ALL="C"
  	export ALLOW_MISSING_DEPENDENCIES=true
