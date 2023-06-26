dacffa3ee7637788d355c1f16e607cb24a85ce45
Adasi Kaakyire <skyhuppa@hotmail.co.uk>
2023-06-26 15:38:49 -0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -34,63 +34,51 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
    fox_get_target_device
 fi
 
-if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-   	export TW_DEFAULT_LANGUAGE="en"
-	export LC_ALL="C"
- 	export ALLOW_MISSING_DEPENDENCIES=true
-	export TARGET_DEVICE_ALT="RMX3085"
-	export OF_TARGET_DEVICES="RMX3085"
-	export OF_VIRTUAL_AB_DEVICE=1
-	export OF_AB_DEVICE=1
-	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
-	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-	export OF_USE_GREEN_LED=0
-	export OF_FBE_METADATA_MOUNT_IGNORE=1
+# Dirty Fix: Only declare orangefox vars when needed
+if [ -f "$(gettop)/bootable/recovery/orangefox.cpp" ]; then
+	echo -e "\x1b[96m[INFO]: Setting up OrangeFox build vars for RMX3085...\x1b[m"
+	if [ "$1" = "$FDEVICE" ] || [  "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+		# Version / Maintainer infos
+		export OF_MAINTAINER="Skyhuppa"
+		export FOX_VERSION=R11.1_1
+		export FOX_BUILD_TYPE="Beta"
 
-	export OF_USE_MAGISKBOOT=1
-	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-	export OF_NO_MIUI_PATCH_WARNING=1
-	export FOX_USE_BASH_SHELL=1
-	export FOX_ASH_IS_BASH=1
-	export FOX_USE_TAR_BINARY=1
-	export FOX_USE_SED_BINARY=1
-	export FOX_USE_XZ_UTILS=1
-	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
-    	export OF_QUICK_BACKUP_LIST="/boot;/data;"
-	export OF_PATCH_AVB20=1
-    	export FOX_DELETE_AROMAFM=1
-    	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
-    	export FOX_ENABLE_APP_MANAGER=0
-    	export FOX_DISABLE_APP_MANAGER=1
+		# Device info
+		export OF_AB_DEVICE=1
+		export OF_VIRTUAL_AB_DEVICE=1
+		export TARGET_DEVICE_ALT="RMX3085"
+		
+		# OTA / DM-Verity / Encryption
+		export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+		export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+		
+		export OF_DONT_PATCH_ON_FRESH_INSTALLATION=1
+		export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+		export OF_KEEP_DM_VERITY_FORCED_ENCRYPTION=1
+		export OF_SKIP_FBE_DECRYPTION_SDKVERSION=34 # Don't try to decrypt A14(?)
+		export OF_SKIP_DECRYPTED_ADOPTED_STORAGE=1
 
-    	# OTA
-    	export OF_KEEP_DM_VERITY=1
-    	export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-    	export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-    	export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+		# Display / Leds
+		export OF_SCREEN_H="2400"
+		export OF_STATUS_H="100"
+		export OF_STATUS_INDENT_LEFT=48
+		export OF_STATUS_INDENT_RIGHT=48
+		export OF_HIDE_NOTCH=1
+		export OF_CLOCK_POS=1 # left and right clock positions available
+		export OF_USE_GREEN_LED=0
+		export OF_FL_PATH1="/tmp/flashlight" # See /init.recovery.mt6785.rc for more information
 
-	# Screen Settings
-	export OF_SCREEN_H=2400
-	export OF_STATUS_H=110
-	export OF_STATUS_INDENT_LEFT=48
-	export OF_STATUS_INDENT_RIGHT=48
-	export OF_CLOCK_POS=1
-	export OF_HIDE_NOTCH=1
-	export OF_ALLOW_DISABLE_NAVBAR=0
-	
-	# R11.1 Settings
-	export FOX_VERSION="R11.1"
-	export OF_MAINTAINER="Skyhuppa"
-	export FOX_VARIANT="A11"
-
+		# Other OrangeFox configs
+		export OF_ENABLE_LPTOOLS=1
+		export OF_ALLOW_DISABLE_NAVBAR=0
+                export OF_QUICK_BACKUP_LIST="/boot;/data;"
+		export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800" # Tue Jan 1 2019 00:00:00 GMT
+		export FOX_DELETE_AROMAFM=1
+  
 	# let's see what are our build VARs
-	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
- 	fi
-fi
+    if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	        export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	        export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+   	        export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  	        export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+    fi
