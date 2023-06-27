ee06b686e9cc32de8e6be0e93dce055fbcc47b1a
Adasi Kaakyire <skyhuppa@hotmail.co.uk>
2023-06-27 10:47:55 -0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,11 +1,13 @@
 #
-# Copyright (C) 2020 The TwrpBuilder Open-Source Project
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,28 +16,79 @@
 # limitations under the License.
 #
 
-# Configure SDCard replacement functionality
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+LOCAL_PATH := device/realme/RMX3085
 
-# SHIPPING API
-PRODUCT_SHIPPING_API_LEVEL := 30
+# A/B
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    lk \
+    preloader \
+    product \
+    system \
+    vbmeta \
+    vbmeta_system \
+    vbmeta_vendor \
+    vendor \
+    vendor_boot
+    
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
 
-# VNDK API
+# V A/B
+ENABLE_VIRTUAL_AB := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Dynam
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# VNDK
 PRODUCT_TARGET_VNDK_VERSION := 31
 
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    $(DEVICE_PATH)
+# API
+PRODUCT_SHIPPING_API_LEVEL := 30
 
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31
 
-# otacert
-# PRODUCT_EXTRA_RECOVERY_KEYS += \
-#    $(DEVICE_PATH)/security/realme_releasekey
+# Health Hal
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
 
-# TWRP_REQUIRED_MODULES += realme_prebuilt \
-#    magisk_prebuilt
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-mtkimpl.recovery
 
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
 
-BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator-service.rosemary
+
+# Copy the vibrator into recovery image
+PRODUCT_COPY_FILES += \
+	$(OUT_DIR)/target/product/rosemary/system/bin/hw/android.hardware.vibrator-service.rosemary:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/hw/android.hardware.vibrator-service.rosemary
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+# MTK PlPath Utils
+PRODUCT_PACKAGES += \
+    mtk_plpath_utils.recovery
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+
