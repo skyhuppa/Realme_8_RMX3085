033c797355bf5b335e7f00bc1ed9fab29749bddb
Adasi Kaakyire <skyhuppa@hotmail.co.uk>
2023-06-27 11:10:23 -0700

    Update twrp_RMX3085.mk

--- twrp_RMX3085.mk
+++ twrp_RMX3085.mk
@@ -1,48 +1,40 @@
 #
-# Copyright (C) 2021 The Android Open Source Project
-# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-
-# Release name
-PRODUCT_RELEASE_NAME := RMX3085
-DEVICE_PATH := device/realme/RMX3085
-
-# Inherit from device.mk configuration
-$(call inherit-product, $(DEVICE_PATH)/device.mk)
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-# $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit some common Omni stuff.
- $(call inherit-product, vendor/twrp/config/common.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit from rosemary device
+$(call inherit-product, device/realme/RMX3085/device.mk)
+
+# Inherit some common TWRP stuff
+$(call inherit-product, vendor/twrp/config/common.mk)
 
   PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root) \
  	 $(LOCAL_PATH)/prebuilt/dtb:dtb.img
- 
+	 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := RMX3085
 PRODUCT_NAME := twrp_RMX3085
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme 8
 PRODUCT_MANUFACTURER := realme
-
-# Dynamic
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-PRODUCT_SHIPPING_API_LEVEL := 30    
-
-# fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
-
-# HACK: Set vendor patch level
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.date.utc=1230768000
-    
-# Assert
-TARGET_OTA_ASSERT_DEVICE := RMX3085,RMX3085_in,RMX3085_gl
